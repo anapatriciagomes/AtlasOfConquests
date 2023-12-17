3d093f239ba73dd896b727a4633284c27c0d06ca
anapatriciagomes <anapatrigomes@gmail.com>
2023-12-17 20:45:13 +0000

    style updates

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,25 @@
 import { Link } from 'react-router-dom';
 import Button from '@mui/material/Button';
+import { styled } from '@mui/material/styles';
+import { orange } from '@mui/material/colors';
 
 function Navbar() {
+  const ColorButton = styled(Button)(({ theme }) => ({
+    color: theme.palette.getContrastText(orange[100]),
+    backgroundColor: orange[100],
+    '&:hover': {
+      backgroundColor: orange[500],
+    },
+  }));
+
   return (
     <nav className="py-4 px-6 fixed top-0 left-0 w-full bg-white">
       <div className="flex justify-between">
         <Link to="/">
-          <Button>Home</Button>
+          <ColorButton>Home</ColorButton>
         </Link>
         <Link to="/login">
-          <Button>Log in</Button>
+          <ColorButton>Log in</ColorButton>
         </Link>
       </div>
     </nav>
