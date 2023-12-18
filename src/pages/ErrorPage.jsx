956562b6edc2f4f6840b01c113bfed3559a8db90
anapatriciagomes <anapatrigomes@gmail.com>
2023-12-18 21:18:02 +0000

    error page update

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -1,5 +1,14 @@
+import catImage from '../assets/cat_404error.png';
+
 function ErrorPage() {
-  return <div>ErrorPage</div>;
+  return (
+    <div>
+      <h1 className="text-4xl text-center mt-[80px] mb-4">
+        Oh no! This page does not exist!
+      </h1>
+      <img src={catImage} alt="cat" />
+    </div>
+  );
 }
 
 export default ErrorPage;
