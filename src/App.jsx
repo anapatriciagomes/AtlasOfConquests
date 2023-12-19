a8bdbbe8ceae2fd3b565f6b3c430334db7a54feb
anapatriciagomes <anapatrigomes@gmail.com>
2023-12-19 16:11:06 +0000

    login page update

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { Routes, Route } from 'react-router-dom';
+import { useState } from 'react';
 import Navbar from './components/Navbar';
 import HomePage from './pages/HomePage';
 import LoginPage from './pages/LoginPage';
@@ -6,12 +7,21 @@ import ErrorPage from './pages/ErrorPage';
 import CountryPage from './pages/CountryPage';
 
 function App() {
+  const [loginPageActive, setLoginPageActive] = useState(false);
+
   return (
     <div className="App">
-      <Navbar />
+      <Navbar
+        loginPageActive={loginPageActive}
+        setLoginPageActive={setLoginPageActive}
+      />
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path="/login" element={<LoginPage />} />
+        <Route
+          path="/login"
+          element={<LoginPage />}
+          setLoginPageActive={setLoginPageActive}
+        />
         <Route path="/country/:countryName" element={<CountryPage />} />
         <Route path="*" element={<ErrorPage />} />
       </Routes>
--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,5 +1,94 @@
-function LoginPage() {
-  return <div>LoginPage</div>;
+import { useState } from 'react';
+import Box from '@mui/material/Box';
+import IconButton from '@mui/material/IconButton';
+import Input from '@mui/material/Input';
+import FilledInput from '@mui/material/FilledInput';
+import OutlinedInput from '@mui/material/OutlinedInput';
+import InputLabel from '@mui/material/InputLabel';
+import InputAdornment from '@mui/material/InputAdornment';
+import FormHelperText from '@mui/material/FormHelperText';
+import FormControl from '@mui/material/FormControl';
+import TextField from '@mui/material/TextField';
+import Visibility from '@mui/icons-material/Visibility';
+import VisibilityOff from '@mui/icons-material/VisibilityOff';
+import AccountCircle from '@mui/icons-material/AccountCircle';
+import Button from '@mui/material/Button';
+import { styled } from '@mui/material/styles';
+import { grey } from '@mui/material/colors';
+
+function LoginPage({ setLoginPageActive }) {
+  const [showPassword, setShowPassword] = useState(false);
+
+  const handleClickShowPassword = () => setShowPassword(show => !show);
+
+  const handleMouseDownPassword = event => {
+    event.preventDefault();
+  };
+
+  const ColorButton = styled(Button)(({ theme }) => ({
+    color: theme.palette.getContrastText(grey[500]),
+    backgroundColor: grey[500],
+    width: '500px',
+    height: '56px',
+    margin: 8,
+
+    '&:hover': {
+      backgroundColor: grey[700],
+      width: '500px',
+      height: '56px',
+      margin: 8,
+    },
+  }));
+
+  return (
+    <div className="w-5/6 mt-[10%] mx-auto">
+      <Box
+        sx={{
+          display: 'flex',
+          flexWrap: 'wrap',
+          flexDirection: 'column',
+          alignItems: 'center',
+        }}
+      >
+        <FormControl sx={{ m: 1, width: '500px' }} variant="outlined">
+          <InputLabel htmlFor="outlined-adornment-email">Email</InputLabel>
+          <OutlinedInput
+            id="outlined-adornment-email"
+            endAdornment={
+              <InputAdornment position="end">
+                <AccountCircle />
+              </InputAdornment>
+            }
+            label="Email"
+          />
+        </FormControl>
+
+        <FormControl sx={{ m: 1, width: '500px' }} variant="outlined">
+          <InputLabel htmlFor="outlined-adornment-password">
+            Password
+          </InputLabel>
+          <OutlinedInput
+            id="outlined-adornment-password"
+            type={showPassword ? 'text' : 'password'}
+            endAdornment={
+              <InputAdornment position="end">
+                <IconButton
+                  aria-label="toggle password visibility"
+                  onClick={handleClickShowPassword}
+                  onMouseDown={handleMouseDownPassword}
+                  edge="end"
+                >
+                  {showPassword ? <VisibilityOff /> : <Visibility />}
+                </IconButton>
+              </InputAdornment>
+            }
+            label="Password"
+          />
+        </FormControl>
+        <ColorButton>Log in</ColorButton>
+      </Box>
+    </div>
+  );
 }
 
 export default LoginPage;
