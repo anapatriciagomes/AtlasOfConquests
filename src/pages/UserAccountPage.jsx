9274b4771b455a2231860dccd3f5964b383367ba
anapatriciagomes <anapatrigomes@gmail.com>
2023-12-21 11:05:00 +0000

    user account page update

--- src/pages/UserAccountPage.jsx
+++ src/pages/UserAccountPage.jsx
@@ -21,8 +21,6 @@ import { red } from '@mui/material/colors';
 import * as React from 'react';
 import Dialog from '@mui/material/Dialog';
 import DialogActions from '@mui/material/DialogActions';
-import DialogContent from '@mui/material/DialogContent';
-import DialogContentText from '@mui/material/DialogContentText';
 import DialogTitle from '@mui/material/DialogTitle';
 import useMediaQuery from '@mui/material/useMediaQuery';
 import { useTheme } from '@mui/material/styles';
