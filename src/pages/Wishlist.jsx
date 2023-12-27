aa01136d6f612fb8168135009527af4cb3862a6c
anapatriciagomes <anapatrigomes@gmail.com>
2023-12-27 20:45:19 +0000

    wishlist page update

--- src/pages/Wishlist.jsx
+++ src/pages/Wishlist.jsx
@@ -1,5 +1,81 @@
-function Wishlist() {
-  return <div>Wishlist</div>;
+import { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
+import CountryCodeConverter from '../components/CountryCodeConverter';
+
+function Wishlist({
+  loggedIn,
+  loggedUserDetails,
+  setLoggedUserDetails,
+  userId,
+}) {
+  const [wishlistCountries, setWishlistCountries] = useState(null);
+  const [countriesFlags, setCountriesFlags] = useState(null);
+
+  useEffect(() => {
+    if (loggedUserDetails && loggedUserDetails.wishlist) {
+      setWishlistCountries(loggedUserDetails.wishlist);
+      console.log(loggedUserDetails.wishlist);
+    }
+  }, [loggedUserDetails]);
+
+  useEffect(() => {
+    axios
+      .get('https://restcountries.com/v3.1/all?fields=name,cca2,flags')
+      .then(response => {
+        setCountriesFlags(response.data);
+        console.log(response.data);
+      })
+      .catch(error => {
+        console.error('Error fetching data:', error);
+      });
+  }, []);
+
+  return (
+    <div>
+      {loggedIn && (
+        <div className="mt-[120px] ml-[80px]">
+          <h1 className="mb-[40px] text-xl text-center">Wishlist Countries</h1>
+          {wishlistCountries ? (
+            wishlistCountries.map(wishlist => (
+              <div key={wishlist.id} className="flex items-center mb-[20px]">
+                <Link
+                  className={`flex text-[${`#ef914e`}] font-semibold hover:text-[#F53]`}
+                  to={`/country/${CountryCodeConverter({
+                    countryName: wishlist.country,
+                  })}/${wishlist.country.replaceAll(' ', '-')}`}
+                >
+                  {countriesFlags
+                    ? countriesFlags
+                        .filter(
+                          flag =>
+                            flag.cca2 ===
+                            CountryCodeConverter({
+                              countryName: wishlist.country,
+                            })
+                        )
+                        .map((flag, index) => (
+                          <img
+                            src={flag.flags.png}
+                            key={index}
+                            className="max-w-[40px] mr-[10px]"
+                          />
+                        ))
+                    : ''}
+
+                  <p>{wishlist.country}</p>
+                </Link>
+              </div>
+            ))
+          ) : (
+            <h1>
+              Your list is still empty, click on the map below to add countries.
+            </h1>
+          )}
+        </div>
+      )}
+    </div>
+  );
 }
 
 export default Wishlist;
