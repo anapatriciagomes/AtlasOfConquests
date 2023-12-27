1aaddbe1dc92dbbd483577a6b9d1019c4b13ccec
anapatriciagomes <anapatrigomes@gmail.com>
2023-12-27 20:41:38 +0000

    visited page update

--- src/pages/Visited.jsx
+++ src/pages/Visited.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
+import axios from 'axios';
 import CountryCodeConverter from '../components/CountryCodeConverter';
 
 function Visited({
@@ -9,6 +10,7 @@ function Visited({
   userId,
 }) {
   const [visitedCountries, setVisitedCountries] = useState(null);
+  const [countriesFlags, setCountriesFlags] = useState(null);
 
   useEffect(() => {
     if (loggedUserDetails && loggedUserDetails.visited) {
@@ -17,21 +19,53 @@ function Visited({
     }
   }, [loggedUserDetails]);
 
+  useEffect(() => {
+    axios
+      .get('https://restcountries.com/v3.1/all?fields=name,cca2,flags')
+      .then(response => {
+        setCountriesFlags(response.data);
+        console.log(response.data);
+      })
+      .catch(error => {
+        console.error('Error fetching data:', error);
+      });
+  }, []);
+
   return (
     <div>
       {loggedIn && (
         <div className="mt-[120px] ml-[80px]">
+          <h1 className="mb-[40px] text-xl text-center">Visited Countries</h1>
           {visitedCountries ? (
             visitedCountries.map(visited => (
-              <Link
-                key={visited.id}
-                className={`mb-[20px] flex flex-col text-[${`#ef914e`}] font-semibold hover:text-[#F53]`}
-                to={`/country/${CountryCodeConverter({
-                  countryName: visited.country,
-                })}/${visited.country.replaceAll(' ', '-')}`}
-              >
-                {visited.country}
-              </Link>
+              <div key={visited.id} className="flex items-center mb-[20px]">
+                <Link
+                  className={`flex text-[${`#ef914e`}] font-semibold hover:text-[#F53]`}
+                  to={`/country/${CountryCodeConverter({
+                    countryName: visited.country,
+                  })}/${visited.country.replaceAll(' ', '-')}`}
+                >
+                  {countriesFlags
+                    ? countriesFlags
+                        .filter(
+                          flag =>
+                            flag.cca2 ===
+                            CountryCodeConverter({
+                              countryName: visited.country,
+                            })
+                        )
+                        .map((flag, index) => (
+                          <img
+                            src={flag.flags.png}
+                            key={index}
+                            className="max-w-[40px] mr-[10px]"
+                          />
+                        ))
+                    : ''}
+
+                  <p>{visited.country}</p>
+                </Link>
+              </div>
             ))
           ) : (
             <h1>
