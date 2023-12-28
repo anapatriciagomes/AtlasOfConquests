665811514dc9f1db515d43472ae04b09bc0a8503
Ruben M. Abreu <ruben_abreu@icloud.com>
2023-12-28 15:29:40 +0000

    searchbar update

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import mGlass from '../assets/magnifying glass.jpg';
 
 function SearchBar({ searchedCountries }) {
   const [search, setSearch] = useState('');
@@ -9,15 +10,24 @@ function SearchBar({ searchedCountries }) {
   };
 
   return (
-    <div>
-      <label htmlFor='search'>Search</label>
-      <input
-        type='text'
-        name='search'
-        id='search'
-        value={search}
-        onChange={handleSearch}
-      />
+    <div className='relative bg-[#ff9800] p-3'>
+      <label htmlFor='search' className='text-xl'>
+        Search by country name:
+      </label>
+      <div className='relative flex items-center'>
+        <input
+          type='text'
+          name='search'
+          id='search'
+          value={search}
+          onChange={handleSearch}
+          className='h-10 w-full pl-10 rounded-md border border-gray-300 focus:outline-none focus:ring focus:border-blue-500'
+          placeholder='Country Name'
+        />
+        <span className='absolute left-3 top-2'>
+          <img src={mGlass} alt='Search' className='h-5 w-5 text-gray-500' />
+        </span>
+      </div>
     </div>
   );
 }
