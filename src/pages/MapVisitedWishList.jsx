f1cc1f3cd2675dc553b25e610e7fbf33f36d0a61
anapatriciagomes <anapatrigomes@gmail.com>
2024-01-04 17:57:30 +0000

    visited wishlist map updated

--- src/pages/MapVisitedWishList.jsx
+++ src/pages/MapVisitedWishList.jsx
@@ -237,6 +237,11 @@ function MapVisitedWishList({
           </div>
         </div>
       )}
+      {loggedIn === false && (
+        <h1 className="mt-[120px] text-center text-xl">
+          Please log in to visit this page
+        </h1>
+      )}
     </div>
   );
 }
