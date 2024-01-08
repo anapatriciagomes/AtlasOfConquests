2592cf9010f14b07b349437fd5051c78370449bb
Ruben M. Abreu <ruben_abreu@icloud.com>
2024-01-08 19:49:13 +0000

    border update

--- src/components/Borders.jsx
+++ src/components/Borders.jsx
@@ -220,14 +220,14 @@ function Borders({ borders = [] }) {
 
         return (
           <div>
-            <p className="pb-3">
-              <span className="font-semibold">Borders:</span>
+            <p className='pb-3'>
+              <span className='font-semibold'>Borders:</span>
             </p>
-            {borderCountries.length > 6 ? (
+            {borderCountries.length > 2 ? (
               <table>
                 <tbody>
                   <tr>
-                    <td className="flex flex-wrap">
+                    <td className='flex flex-wrap'>
                       {borderCountries.map((country, index) => (
                         <Link
                           key={index}
@@ -249,7 +249,7 @@ function Borders({ borders = [] }) {
                 <tbody>
                   <tr>
                     {borderCountries.map((country, index) => (
-                      <td key={index} className="flex mb-1">
+                      <td key={index} className='flex mb-1'>
                         <Link
                           className={`text-[#ef914e] font-semibold hover:text-[#F53]`}
                           countryName={country}
