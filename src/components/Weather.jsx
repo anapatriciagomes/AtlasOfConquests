694bba1ff831e1e7860fa839a7065d2c89e6db81
anapatriciagomes <anapatrigomes@gmail.com>
2024-01-17 19:45:54 +0000

    weather fix attempt

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -12,41 +12,41 @@ function Weather({ capital }) {
   const [description, setDescription] = useState(null);
   const [humidity, setHumidity] = useState(null);
 
-  useEffect(() => {
-    const fetchWeather = async () => {
-      if (firstCity === 'Washington, D.C.') {
-        firstCity = 'Washington';
-      }
-      let formattedCity = firstCity.replace(/ /g, '.');
-      formattedCity = formattedCity
-        .normalize('NFD')
-        .replace(/[\u0300-\u036f]/g, '');
-      try {
-        const weather = new OpenWeatherAPI({
-          key: `${import.meta.env.VITE_API_WEATHER_KEY}`,
-          locationName: encodeURIComponent(formattedCity),
-          units: 'metric',
-        });
-        const data = await weather.getCurrent();
-        setFetching(false);
-        setCurrentTemperature(data.weather.temp.cur);
-        setCurrentConditions(data.weather.icon.url);
-        setFeelsLike(data.weather.feelsLike.cur);
-        setDescription(data.weather.description);
-        setHumidity(data.weather.humidity);
-      } catch (error) {
-        console.error('Error fetching weather:', error);
-        setFetching(false);
-      }
-    };
+  const fetchWeather = async () => {
+    if (firstCity === 'Washington, D.C.') {
+      firstCity = 'Washington';
+    }
+    let formattedCity = firstCity.replace(/ /g, '.');
+    formattedCity = formattedCity
+      .normalize('NFD')
+      .replace(/[\u0300-\u036f]/g, '');
+    try {
+      const weather = new OpenWeatherAPI({
+        key: `${import.meta.env.VITE_API_WEATHER_KEY}`,
+        locationName: encodeURIComponent(formattedCity),
+        units: 'metric',
+      });
+      const data = await weather.getCurrent();
+      setFetching(false);
+      setCurrentTemperature(data.weather.temp.cur);
+      setCurrentConditions(data.weather.icon.url);
+      setFeelsLike(data.weather.feelsLike.cur);
+      setDescription(data.weather.description);
+      setHumidity(data.weather.humidity);
+    } catch (error) {
+      console.error('Error fetching weather:', error);
+      setFetching(false);
+    }
+  };
 
+  useEffect(() => {
     fetchWeather();
   }, [firstCity]);
 
   return (
     <div>
       {fetching && (
-        <div className='mt-[80px] text-center'>
+        <div className="mt-[80px] text-center">
           <Box
             sx={{
               display: 'flex',
@@ -62,18 +62,18 @@ function Weather({ capital }) {
       <>
         {currentTemperature !== null ? (
           <>
-            <div className='flex items-center'>
+            <div className="flex items-center">
               {' '}
               <img
-                className='w-[50px]'
+                className="w-[50px]"
                 src={currentConditions}
-                alt='weather conditions'
+                alt="weather conditions"
               />
-              <span className='text-xl'>
+              <span className="text-xl">
                 {Math.round(currentTemperature)}°C
               </span>
             </div>{' '}
-            <div className='text-xs'>
+            <div className="text-xs">
               <p>
                 Feels like {feelsLike.toFixed(1)}°C |{' '}
                 {description.charAt(0).toUpperCase() + description.slice(1)} |
