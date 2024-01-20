bbcba24208ca247246d8bc4bd7efb91b1adfffe4
Ruben M. Abreu <ruben_abreu@icloud.com>
2024-01-20 13:06:32 +0000

    working on distance calculator

---
+++ src/components/DistanceCalculator.jsx
@@ -0,0 +1,100 @@
+import { useEffect, useState } from 'react';
+
+const MyLocation = ({ position }) => {
+  return (
+    <div className='mt-[200px]'>
+      <h2>My Current Location</h2>
+      {position.latitude && position.longitude ? (
+        <p>
+          Latitude: {position.latitude}, Longitude: {position.longitude}
+        </p>
+      ) : (
+        <p>Please enable your location</p>
+      )}
+    </div>
+  );
+};
+
+function DistanceCalculator({ lat, lng }) {
+  const [position, setPosition] = useState({
+    latitude: null,
+    longitude: null,
+  });
+
+  const [distances, setDistances] = useState({
+    kilometers: 0,
+    miles: 0,
+  });
+
+  function haversineDistance(coords1, coords2) {
+    function toRad(x) {
+      return (x * Math.PI) / 180;
+    }
+
+    var lon1 = coords1.lon;
+    var lat1 = coords1.lat;
+
+    var lon2 = coords2.lon;
+    var lat2 = coords2.lat;
+
+    var R = 6371; // Earth radius in kilometers
+    var MILES_CONVERSION = 0.621371;
+
+    var x1 = lat2 - lat1;
+    var dLat = toRad(x1);
+    var x2 = lon2 - lon1;
+    var dLon = toRad(x2);
+    var a =
+      Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+      Math.cos(toRad(lat1)) *
+        Math.cos(toRad(lat2)) *
+        Math.sin(dLon / 2) *
+        Math.sin(dLon / 2);
+    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+    var distanceInKilometers = R * c;
+    var distanceInMiles = distanceInKilometers * MILES_CONVERSION;
+
+    return {
+      kilometers: distanceInKilometers,
+      miles: distanceInMiles,
+    };
+  }
+
+  useEffect(() => {
+    if ('geolocation' in navigator) {
+      navigator.geolocation.getCurrentPosition(
+        function (position) {
+          setPosition({
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude,
+          });
+        },
+        function (error) {
+          console.error('Error getting geolocation:', error.message);
+        }
+      );
+    } else {
+      console.log('Geolocation is not available in your browser.');
+    }
+  }, []);
+
+  useEffect(() => {
+    const coords1 = { lat: position.latitude, lon: position.longitude };
+    const coords2 = { lat: lat, lon: lng };
+    const newDistances = haversineDistance(coords1, coords2);
+    setDistances(newDistances);
+  }, [lat, lng, position]);
+
+  return (
+    <div>
+      <MyLocation position={position} />
+      {`You are ${distances.kilometers.toFixed(
+        1
+      )} kilometers (${distances.miles.toFixed(
+        1
+      )} miles) from this country's center`}
+    </div>
+  );
+}
+
+export default DistanceCalculator;
