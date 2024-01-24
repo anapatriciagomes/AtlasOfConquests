0aaec900960c81c8295bcb9770234c41e694e98f
anapatriciagomes <anapatrigomes@gmail.com>
2024-01-24 18:55:46 +0000

    vite.config.js updated

--- vite.config.js
+++ vite.config.js
@@ -13,6 +13,32 @@ const manifestForPlugIn = {
     name: 'Atlas of Conquests',
     short_name: 'Atlas',
     description: 'Atlas of Conquests PWA',
+    icons: [
+      {
+        src: './src/assets/world_atlas_favicon_192x192.png',
+        sizes: '192x192',
+        type: 'image/png',
+        purpose: 'favicon',
+      },
+      {
+        src: './src/assets/world_atlas_favicon_512x512.png',
+        sizes: '512x512',
+        type: 'image/png',
+        purpose: 'favicon',
+      },
+      {
+        src: './src/assets/world_atlas_favicon_apple_180x180.png',
+        sizes: '180x180',
+        type: 'image/png',
+        purpose: 'apple-touch-icon',
+      },
+      {
+        src: './src/assets/world_atlas_favicon_512x512.png',
+        sizes: '512x512',
+        type: 'image/png',
+        purpose: 'any maskable',
+      },
+    ],
     theme_color: '#ffffff',
     background_color: '#f0e7db',
     display: 'standalone',
