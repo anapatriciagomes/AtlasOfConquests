5dacc6bec68faa7bf26ee1fd6d8602ea4e813ef3
anapatriciagomes <anapatrigomes@gmail.com>
2024-01-24 18:21:44 +0000

    vite.config.js updated

--- vite.config.js
+++ vite.config.js
@@ -4,26 +4,30 @@ import { VitePWA } from 'vite-plugin-pwa';
 
 const manifestForPlugIn = {
   registerType: 'prompt',
-  includeAssests:["./src/assets/world_atlas_favicon_192x192.png", "./src/assets/world_atlas_favicon_512x512.png", "./src/assets/world_atlas_favicon_apple_180x180.png"],
+  includeAssests: [
+    './src/assets/world_atlas_favicon_192x192.png',
+    './src/assets/world_atlas_favicon_512x512.png',
+    './src/assets/world_atlas_favicon_apple_180x180.png',
+  ],
   manifest: {
-    name: "Atlas of Conquests",
-    short_name: "Atlas",
-    description: "Atlas of Conquests PWA",
+    name: 'Atlas of Conquests',
+    short_name: 'Atlas',
+    description: 'Atlas of Conquests PWA',
     icons: [
       {
-        src: "./src/assets/world_atlas_favicon_192x192.png",
+        src: './src/assets/world_atlas_favicon_192x192.png',
         sizes: '192x192',
         type: 'image/png',
         purpose: 'any',
       },
       {
-        src: "./src/assets/world_atlas_favicon_512x512.png",
+        src: './src/assets/world_atlas_favicon_512x512.png',
         sizes: '512x512',
         type: 'image/png',
         purpose: 'any maskable',
       },
       {
-        src: "./src/assets/world_atlas_favicon_apple_180x180.png",
+        src: './src/assets/world_atlas_favicon_apple_180x180.png',
         sizes: '180x180',
         type: 'image/png',
         purpose: 'apple-touch-icon',
@@ -31,9 +35,9 @@ const manifestForPlugIn = {
     ],
     theme_color: '#ffffff',
     background_color: '#f0e7db',
-    display: "standalone",
+    display: 'standalone',
     scope: '/',
-    start_url: "/",
+    start_url: '/',
     orientation: 'portrait',
     prefer_related_applications: false,
   },
@@ -41,4 +45,4 @@ const manifestForPlugIn = {
 
 export default defineConfig({
   plugins: [react(), VitePWA(manifestForPlugIn)],
-})
+});
