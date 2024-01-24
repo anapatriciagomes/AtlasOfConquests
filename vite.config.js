c02cc9b2247fe04ef3de8556481bd0a443eec47d
Ana Patrícia Gomes <anapatrigomes@gmail.com>
2024-01-24 12:54:51 +0000

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,29 +2,47 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { VitePWA } from 'vite-plugin-pwa';
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    react(),
-    VitePWA({
-      manifest: {
-        name: 'Atlas of Conquests',
-        short_name: 'Atlas',
-        description: 'Atlas of Conquests PWA',
-        theme_color: '#ffffff',
-        icons: [
-          {
-            src: './src/assets/world_atlas_favicon.png',
-            sizes: '96x96',
-            type: 'image/png',
-          },
-        ],
-      },
-    }),
+const manifestForPlugIn = {
+  registerType:'prompt',
+  includeAssests:['favicon.ico', "apple-touc-icon.png", "masked-icon.svg"],
+  manifest:{
+    name:"Atlas of Conquests",
+    short_name:"Atlas",
+    description:"Atlas of Conquests PWA",
+    icons:[{
+      src: './src/assets/world_atlas_favicon_192x192.png',
+      sizes:'192x192',
+      type:'image/png',
+      purpose:'favicon'
+    },
+    {
+      src:'./src/assets/world_atlas_favicon_512x512.png',
+      sizes:'512x512',
+      type:'image/png',
+      purpose:'favicon'
+    },
+    {
+      src: './src/assets/world_atlas_favicon_apple_180x180.png',
+      sizes:'180x180',
+      type:'image/png',
+      purpose:'apple touch icon',
+    },
+    {
+      src: './src/assets/world_atlas_favicon_512x512.png',
+      sizes:'512x512',
+      type:'image/png',
+      purpose:'any maskable',
+    }
   ],
-  test: {
-    globals: true,
-    environment: 'jsdom',
-    setupFiles: './src/tests/setup.js',
-  },
-});
+  theme_color:'#ffffff',
+  background_color:'#f0e7db',
+  display:"standalone",
+  scope:'/',
+  start_url:"/",
+  orientation:'portrait'
+  }
+}
+
+export default defineConfig({
+  plugins: [react(), VitePWA(manifestForPlugIn)],
+})
