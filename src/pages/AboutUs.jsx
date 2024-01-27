6cc67964419b18774ffc2085327e8fbd3586e516
anapatriciagomes <anapatrigomes@gmail.com>
2024-01-27 14:28:37 +0000

    style update

--- src/pages/AboutUs.jsx
+++ src/pages/AboutUs.jsx
@@ -13,85 +13,87 @@ function AboutUs({ darkMode }) {
         backgroundSize: 'cover',
         backgroundPosition: 'center',
       }}
-      className='w-[100vw] h-[100vh] pt-[160px] sm: pb-[750px]'
+      className="w-[100vw] h-[100vh] pt-[110px] max-[640px]:pt-[100px] max-[640px]:pb-[30px]"
     >
       <div
         className={` ${
           darkMode ? 'bg-[#222222]' : 'bg-white'
-        } w-[90%] mx-auto bg-opacity-70 p-4 rounded-md `}
+        } w-[90%] mx-auto bg-opacity-70 p-[30px] rounded-md max-[640px]:p-[20px]`}
       >
-        <h1 className={`text-xl text-center mb-[30px] mt-[0px] `}>About Us</h1>
-        <div
-          className={`grid grid-cols-1 sm:grid-cols-2 gap-10 justify-items-center `}
+        <h1
+          className={`text-xl text-center mb-[30px] mt-[0px] max-[640px]:mb-[20px]`}
         >
-          <div className='sm:mr-4 mb-4 sm:mb-0'>
-            <h2 className='text-lg font-medium'>Ana Patrícia Gomes</h2>
+          About Us
+        </h1>
+        <div
+          className={`flex justify-center max-[640px]:flex-col max-[640px]:items-center`}
+        >
+          <div className="text-center w-[50%] max-[640px]:mr-0 max-[640px]:mb-[15px] max-[640px]:w-[95%]">
+            <h2 className="text-lg font-medium">Ana Patrícia Gomes</h2>
             <img
               src={Patricia}
-              alt='Patrícia'
-              className='mx-auto max-w-[100px] mt-[35px]'
+              alt="Patrícia"
+              className="mx-auto max-w-[100px] mt-[35px] max-[640px]:mt-[15px]"
             />
-            <div className='mt-[30px]'>
+            <div className="mt-[30px] max-[640px]:mt-[15px]">
               <LocationOnIcon />
               <a
-                href='https://maps.app.goo.gl/p21MQCrtdEj5Ws8s6'
-                target='_blank'
-                rel='noopener noreferrer'
+                href="https://maps.app.goo.gl/p21MQCrtdEj5Ws8s6"
+                target="_blank"
+                rel="noopener noreferrer"
               >
                 &nbsp; Lisbon, Portugal
               </a>
             </div>
-            <div className='mt-[30px]'>
+            <div className="mt-[30px] max-[640px]:mt-[15px] ">
               <a
-                href='https://github.com/anapatriciagomes'
-                target='_blank'
-                rel='noopener noreferrer'
+                href="https://github.com/anapatriciagomes"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="mr-[70px] max-[250px]:mr-[20px]"
               >
-                <GitHubIcon
-                  style={{ width: '42px', height: '42px', marginRight: '70px' }}
-                />
+                <GitHubIcon style={{ width: '42px', height: '42px' }} />
               </a>
               <a
-                href='https://www.linkedin.com/in/anapatriciagomes/'
-                target='_blank'
-                rel='noopener noreferrer'
+                href="https://www.linkedin.com/in/anapatriciagomes/"
+                target="_blank"
+                rel="noopener noreferrer"
               >
                 <LinkedInIcon style={{ width: '42px', height: '42px' }} />
               </a>
             </div>
           </div>
 
-          <div className='text-center sm:text-left '>
-            <h2 className='text-lg font-medium text-center'>Ruben Abreu</h2>
+          <div className="text-center w-[50%] max-[640px]:ml-0 max-[640px]:mt-[15px] max-[640px]:w-[95%]">
+            <h2 className="text-lg font-medium text-center">Ruben Abreu</h2>
             <img
               src={Ruben}
-              alt='Ruben'
-              className='mx-auto max-w-[100px] mt-[35px]'
+              alt="Ruben"
+              className="mx-auto max-w-[100px] mt-[35px] max-[640px]:mt-[15px]"
             />
-            <div className='mt-[30px]'>
+            <div className="mt-[30px] max-[640px]:mt-[15px]">
               <LocationOnIcon />
               <a
-                href='https://maps.app.goo.gl/p21MQCrtdEj5Ws8s6'
-                target='_blank'
-                rel='noopener noreferrer'
+                href="https://maps.app.goo.gl/p21MQCrtdEj5Ws8s6"
+                target="_blank"
+                rel="noopener noreferrer"
               >
                 &nbsp; Lisbon, Portugal
               </a>
             </div>
-            <div className='mt-[30px]'>
+            <div className="mt-[30px] max-[640px]:mt-[20px]">
               <a
-                href='https://github.com/ruben-abreu'
-                target='_blank'
-                rel='noopener noreferrer'
+                href="https://github.com/ruben-abreu"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="mr-[70px] max-[250px]:mr-[20px]"
               >
-                <GitHubIcon
-                  style={{ width: '42px', height: '42px', marginRight: '70px' }}
-                />
+                <GitHubIcon style={{ width: '42px', height: '42px' }} />
               </a>
               <a
-                href='https://www.linkedin.com/in/ruben-abreu1/'
-                target='_blank'
-                rel='noopener noreferrer'
+                href="https://www.linkedin.com/in/ruben-abreu1/"
+                target="_blank"
+                rel="noopener noreferrer"
               >
                 <LinkedInIcon style={{ width: '42px', height: '42px' }} />
               </a>
