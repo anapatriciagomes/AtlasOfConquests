6e90033ed83ad7e0721e39ec9b4024b362f789a9
Ruben M. Abreu <ruben_abreu@icloud.com>
2024-01-27 11:45:32 +0000

    about page update

--- src/pages/AboutUs.jsx
+++ src/pages/AboutUs.jsx
@@ -8,25 +8,33 @@ function AboutUs() {
   return (
     <div className='mt-[120px]'>
       <h1 className='text-xl text-center my-[30px]'>About Us</h1>
-      <div className='grid grid-cols-2 justify-items-center'>
-        <div>
-          <h2 className='text-lg'>Ana Patrícia Gomes</h2>
-          <img src={Patricia} alt='Patrícia' className='w-[100px]' />
-          <LocationOnIcon />
-          <a
-            href='https://maps.app.goo.gl/p21MQCrtdEj5Ws8s6'
-            target='_blank'
-            rel='noopener noreferrer'
-          >
-            &nbsp; Lisbon, Portugal
-          </a>
-          <p>
+      <div className='grid grid-cols-1 sm:grid-cols-2 gap-10 justify-items-center'>
+        <div className='sm:mr-4 mb-4 sm:mb-0'>
+          <h2 className='text-lg font-medium'>Ana Patrícia Gomes</h2>
+          <img
+            src={Patricia}
+            alt='Patrícia'
+            className='mx-auto max-w-[100px] mt-[35px]'
+          />
+          <div className='mt-[30px]'>
+            <LocationOnIcon />
+            <a
+              href='https://maps.app.goo.gl/p21MQCrtdEj5Ws8s6'
+              target='_blank'
+              rel='noopener noreferrer'
+            >
+              &nbsp; Lisbon, Portugal
+            </a>
+          </div>
+          <p className='mt-[30px]'>
             <a
               href='https://github.com/anapatriciagomes'
               target='_blank'
               rel='noopener noreferrer'
             >
-              <GitHubIcon style={{ width: '42px', height: '42px' }} />
+              <GitHubIcon
+                style={{ width: '42px', height: '42px', marginRight: '70px' }}
+              />
             </a>
             <a
               href='https://www.linkedin.com/in/anapatriciagomes/'
@@ -37,22 +45,39 @@ function AboutUs() {
             </a>
           </p>
         </div>
-        <div>
-          <h2 className='text-lg'>Ruben Abreu</h2>
-          <img src={Ruben} alt='Ruben' className='w-[100px]' />
-          <LocationOnIcon />
-          <a
-            href='https://maps.app.goo.gl/p21MQCrtdEj5Ws8s6'
-            target='_blank'
-            rel='noopener noreferrer'
-          >
-            &nbsp; Lisbon, Portugal
-          </a>
-          <p>
-            <a href='https://github.com/ruben-abreu'>
-              <GitHubIcon style={{ width: '42px', height: '42px' }} />
+
+        <div className='text-center sm:text-left '>
+          <h2 className='text-lg font-medium text-center'>Ruben Abreu</h2>
+          <img
+            src={Ruben}
+            alt='Ruben'
+            className='mx-auto max-w-[100px] mt-[35px]'
+          />
+          <div className='mt-[30px]'>
+            <LocationOnIcon />
+            <a
+              href='https://maps.app.goo.gl/p21MQCrtdEj5Ws8s6'
+              target='_blank'
+              rel='noopener noreferrer'
+            >
+              &nbsp; Lisbon, Portugal
             </a>
-            <a href='https://www.linkedin.com/in/ruben-abreu1/'>
+          </div>
+          <p className='mt-[30px]'>
+            <a
+              href='https://github.com/ruben-abreu'
+              target='_blank'
+              rel='noopener noreferrer'
+            >
+              <GitHubIcon
+                style={{ width: '42px', height: '42px', marginRight: '70px' }}
+              />
+            </a>
+            <a
+              href='https://www.linkedin.com/in/ruben-abreu1/'
+              target='_blank'
+              rel='noopener noreferrer'
+            >
               <LinkedInIcon style={{ width: '42px', height: '42px' }} />
             </a>
           </p>
